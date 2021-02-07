bd7b7e1c7ab04cbc21ef0c0295c969ef55297078
Rodolfo Vieira <rodolfovieira95@outlook.com>
2021-02-07 18:23:48 -0300

    housekeeping: update navbar dinâmico

--- ge-calendar/src/components/Navbar/component.tsx
+++ ge-calendar/src/components/Navbar/component.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { NavbarType } from './interface';
 import { Link, NavBarComponent, NavBarContent, Title } from './style';
 
-export const NavBar = () => {
+export const NavBar = ({ actualDate, updateData }: NavbarType) => {
+    const [dateTime, setDateTime] = useState<Date>(new Date());
     const anterior = "<"
     const posterior = ">"
-    
-    return (<NavBarComponent> 
+
+    const handleClick = (button: string) => {
+        button === "anterior"
+            ? dateTime?.setDate(dateTime.getDate() - 1)
+            : dateTime?.setDate(dateTime.getDate() + 1)
+        const teste = dateTime?.getTime();
+        updateData(dateTime.toISOString().split('T')[0])
+        setDateTime(new Date(teste));
+    }
+
+    const dayHandler = () => {
+        if (actualDate.getDate() === dateTime?.getDate()) {
+            return "Hoje"
+        }
+        if (actualDate.getDate() === (dateTime?.getDate() + 1)) {
+            return "Ontem"
+        }
+        if (actualDate.getDate() === (dateTime?.getDate() - 1)) {
+            return "Amanhã"
+        }
+        return dateTime?.toLocaleDateString()
+    }
+
+    return (<NavBarComponent>
         <NavBarContent>
-            <Link href="#" >{anterior}</Link>
-            <Title>Ontem</Title>
-            <Link href="#">{posterior}</Link>
+            <Link href="#" onClick={() => handleClick("anterior")} >{anterior}</Link>
+            <Title>{dayHandler()}</Title>
+            <Link href="#" onClick={() => handleClick("posterior")}>{posterior}</Link>
         </NavBarContent>
     </NavBarComponent>)
 }
---
+++ ge-calendar/src/components/Navbar/interface.ts
@@ -0,0 +1,4 @@
+export type NavbarType = {
+    actualDate: Date,
+    updateData: Function
+}
