fc1b23058688e7b9e123ce647e9bacf84bfaeb06
Rodolfo Vieira <rodolfovieira95@outlook.com>
2021-02-14 22:28:04 -0300

    housekeeping: alteração da lógica do Calendar

--- ge-calendar/src/pages/Calendar/component.tsx
+++ ge-calendar/src/pages/Calendar/component.tsx
@@ -1,44 +1,53 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import { CampeonatoTitle } from './style';
 
 import { Card } from '../../components/Card/index';
 import { CampeonatoContainer, CardContainer, HigherContainer } from '../../components/Container';
 import { NavBar } from '../../components/Navbar';
-import { getData } from '../../api';
-import { MockDataType } from '../../api/interface';
+import useFetch from '../../hooks/useFetch/index'
+import { Error } from '../../components/Error'
+import { Loading } from '../../components/Loading'
 
 export const Calendar = () => {
-    const [data, setData] = useState<MockDataType>();
-    const [date, setDate] = useState<Date>(new Date());
-
-    useEffect(() => {
+    const [date, setDate] = useState<Date>(() => {
         const newDate = new Date()
-        setDate(newDate)
-        setData(getData(newDate.toISOString().split('T')[0]))
-    }, [])
+        newDate.setFullYear(2019)
+        return newDate
+    });
+    const { status, data, statusCode } = useFetch(date);
 
-    const updateData = (date: string): void => {
-        setData(getData(date))
-    }
-
-    return !data
-        ? <div>Carregando...</div>
-        : (
-            <>
-                <NavBar actualDate={date} updateData={updateData} />
-                <HigherContainer>
-                    {data?.campeonatos && data?.campeonatos.map((camp) =>
-                    (<CampeonatoContainer key={camp.campeonato_id}>
+    const Component = (
+        <>
+            <NavBar
+                actualDate={date}
+                updateDate={(date: Date) => setDate(date)}
+            />
+            <HigherContainer>
+                {data?.campeonatos && data?.campeonatos.map(camp =>
+                (
+                    <CampeonatoContainer key={camp.campeonato_id}>
                         <CampeonatoTitle>{camp.nome_do_campeonato}</CampeonatoTitle>
                         <CardContainer>
-                            {camp.jogos.map((jogo) => (
-                                <Card key={jogo.jogo_id} jogo={jogo} actualDate={date} />
+                            {camp.jogos.map(jogo => (
+                                <Card
+                                    key={jogo.jogo_id}
+                                    jogo={jogo}
+                                    actualDate={date}
+                                />
                             ))}
                         </CardContainer>
-                    </CampeonatoContainer>)
-                    )}
-                </HigherContainer>
-            </>
-        )
+                    </CampeonatoContainer>
+                )
+                )}
+            </HigherContainer>
+        </>
+    )
+
+    if ((status === "fetched" || status === "error") && statusCode !== 200) {
+        return <Error />
+    } else {
+        return !data ? <Loading /> : Component
+    }
+
 }
