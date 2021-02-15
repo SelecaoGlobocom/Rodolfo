278946937d2469833e9657933882b655d986712d
Rodolfo Vieira <rodolfovieira95@outlook.com>
2021-02-14 22:26:18 -0300

    feature: adição do componente de erro

---
+++ ge-calendar/src/components/Error/component.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { ErrorContainer } from './style'
+
+export const Error = () => (
+    <ErrorContainer>
+        Não haverá jogo nessa data.
+    </ErrorContainer>
+)
